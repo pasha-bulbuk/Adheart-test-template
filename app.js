401edda3c04407abca66c8f8bc76a07770cd1eab
misha-tarnavskyi <mishatarnavskii14@gmail.com>
2023-05-31 21:25:32 +0300

    forrmater

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('burger').addEventListener('click', function() {
-        document.querySelector('header').classList.toggle('open')
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("burger").addEventListener("click", function () {
+    document.querySelector("header").classList.toggle("open");
+  });
+});
