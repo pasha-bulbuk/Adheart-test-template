0cd676e68bd701bcbcae6ab35e2a1f6863588e95
Pavel Bulbuk <none>
2023-07-26 12:34:36 +0200

    fix gulpfile sass require

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
-const sass = require('gulp-sass')(require('sass'));
+// const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('sass-complite', function(){
+gulp.task('sass-compile', function() {
     return gulp.src('./scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().toString('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css/'))
 })
