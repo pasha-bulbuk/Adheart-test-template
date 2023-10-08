054c76bc7e3a88c7a9a9bd8281ddcc36f0219c01
misha-tarnavskyi <mishatarnavskii14@gmail.com>
2023-10-08 13:00:22 +0300

    end 2 section

--- app.js
+++ app.js
@@ -9,3 +9,27 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("main").classList.toggle("open");
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var tabList = document.querySelectorAll("ul.tabs li");
+
+  tabList.forEach(function(tab) {
+    tab.addEventListener("click", function() {
+      var tab_id = this.getAttribute("data-tab");
+
+      tabList.forEach(function(tab) {
+        tab.classList.remove("current");
+      });
+
+      var tabContents = document.querySelectorAll(".tab-content");
+      tabContents.forEach(function(content) {
+        content.classList.remove("current");
+      });
+
+      this.classList.add("current");
+      document.getElementById(tab_id).classList.add("current");
+    });
+  });
+});
+
